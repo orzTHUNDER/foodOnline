15d8a1cd828878f2ad4dcd06037c07bb4bfe2b41
orzTHUNDER <a.r.nandhabeast@gmail.com>
2022-08-20 01:25:42 +0530

    get user's current locations and optimize the home page to show the nearby restaurants

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from vendor.models import Vendor
 
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
+from django.contrib.gis.db.models.functions import Distance
+
+
+
+def get_or_set_current_location(request):
+    if 'lat' in request.session:    #if latitude and longitude is set in the session
+        lat = request.session['lat']
+        lng = request.session['lng']
+        return lng, lat
+    elif 'lat' in request.GET:     #if lat is present in "url-parameter"
+        lat = request.GET.get('lat')  #get('lat')  bacause name of the latitude is lat
+        lng = request.GET.get('lng')
+        request.session['lat'] = lat  #setting the lat and long
+        request.session['lng'] = lng
+        return lng, lat
+    else:
+        return None
+
 def home(request):
-    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+    if get_or_set_current_location(request) is not None:
+        pnt = GEOSGeometry('POINT(%s %s)' % (get_or_set_current_location(request)))
+
+        #get the vendor nearby
+        vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=1000))).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
+
+        for v in vendors:
+            v.kms = round(v.distance.km, 1)
+
+    else:
+        vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+    
     context = {
         'vendors': vendors,
     }     
